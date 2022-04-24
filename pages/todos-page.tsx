4a28543d072dbbc5d444425900fb0f298aa03703
wat1881mayer <happymayer2013@icloud.com>
2022-04-24 23:41:00 +0900

    first commit

---
+++ pages/todos-page.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+
+const TodosPage: React.FC = () => {
+  return (
+    <Layout title="Todos">
+      <p className="text-4xl">todos page</p>
+    </Layout>
+  );
+};
+export default TodosPage;
